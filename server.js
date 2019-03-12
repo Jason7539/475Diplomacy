0d3b28e07a5e7291e2f96572ae26e62a8e65e2e0
sophannaek <47133629+sophannaek@users.noreply.github.com>
2019-03-11 22:00:14 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+var os = require("os");
+
+const port = 3001
+const ip = 'localhost'
+
+let users = [];             // array to hold userNames of clients
+
+// app.get("/", (req, res) => {
+//   res.send("got a get");
+// })
+
+app.post("/", (req, res) => {
+  let body = []
+  console.log("you posted something");
+  req.on("data", (chunk) =>{ // process the username and
+    body.push(chunk);
+
+  }).on("end", () => {
+    let jsonObj = JSON.parse(Buffer.concat(body).toString());
+
+    users.push(jsonObj.userName);     // add the username to the list of usernames
+    console.log("the users so far are ");
+    console.log(users);
+  })
+});
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/map.html');
+});
+
+app.use(express.static('./images'))
+
+app.listen(port, ip, () =>{
+  console.log("connected");
+});
+
+
+// var hosting = http.createServer((request, response) => {
+//   const { headers, method, url } = request;
+//   let body = [];
+//   response.write('<html>');
+//   response.write('<body>');
+//   response.write('<h1>Hey David</h1>');
+//   response.write('</body>');
+//   response.write('</html>');
+//
+//   // responding to a Post request
+//   if (method == "POST"){
+//     console.log("got a post");
+//     request.on("data", (chunk) => {
+//       body.push(chunk);
+//     }).on('end', () => {
+//       jsonObj = JSON.parse(Buffer.concat(body).toString());
+//       //console.log(JSON.parse(jsonObj));
+//
+//       users.push(jsonObj.userName);   // adding client username is list
+//       console.log("just added" + jsonObj.userName);
+//     });
+//   }
+//   response.end();
+// }).listen(port, ip ,()=>{
+//   console.log("server started");
+// }); // Activates this server, listening on port
+// hosting.on("error", (err) => {
+//   console.log(err);
+// });
+
+
+//Handle client trying to join
+// hosting.close();
+// hosting.on("close", () => {
+//   console.log("you closed it");
+// });
