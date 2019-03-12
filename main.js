fb0dfa45963e253e78a0a631457726c792e68529
Vishant <vishantk@gmail.com>
2019-03-12 16:20:10 -0700

    sudoku

--- main.js
+++ main.js
@@ -16,6 +16,8 @@ function createWindow () {
 
   // and load the index.html of the app.
   win.loadFile('index.html')
+    
+  win.setMenuBarVisibility(false)
   // Open the DevTools.
  // win.webContents.openDevTools()
 
