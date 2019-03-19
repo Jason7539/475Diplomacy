037784986519e295b32c632cbf644281393c3dfb
Chelsea Marfil <chelseamarfil@yahoo.com>
2019-03-19 16:26:26 -0700

    Added moveunit function

--- src/map.js
+++ src/map.js
@@ -1,42 +1,58 @@
 
 
+function move(){
+    alert("click country to move to")
+    D = document.getElementById("E")
+    doc = D.getSVGDocument()
+    rus = doc.getElementById("russia")
+    rus.style.fill = "#000000"
+}
 
-    function move(){
-        alert("click country to move to")
-        D = document.getElementById("E")
-        doc = D.getSVGDocument()
-        rus = doc.getElementById("russia")
-        rus.style.fill = "#000000"
-      }
-  
-      function support() {
-        alert("click country to support")
-      }
-  
-      function convoy() {
-        alert("Select army to convoy")
-      }
-  
-      function hold() {
-        alert("Selected country will hold")
-      }
-  
-      /* for chat display */
-       function openForm() {
-        document.getElementById("myForm").style.display = "block";
-      }
-  
-      function closeForm() {
-        document.getElementById("myForm").style.display = "none";
-      }
+function moveunit(id, dest){
+    D = document.getElementById("E")
+    doc = D.getSVGDocument()
+    troop = doc.getElementById(id)
 
-  function Here(id){
+    alert(troop.getAttribute("cx"))
+
+
+    target = doc.getElementById(dest)
+    alert(target)
+
+    targetX = target.getAttribute("cx")
+    troop.setAttribute("cx", targetX)
+
+    // moving
+}
+
+function support() {
+    alert("click country to support")
+}
+
+function convoy() {
+    alert("Select army to convoy")
+}
+
+function hold() {
+    alert("Selected country will hold")
+}
+
+/* for chat display */
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+function Here(id){
     alert(id)
     prv=document.getElementById(id)
     if(prv.style.fill === "rgb(234, 11, 140)"){
-      prv.style.fill = "#3399ff"
+    prv.style.fill = "#3399ff"
     }
     else{
-      prv.style.fill = "#ea0b8c"
+    prv.style.fill = "#ea0b8c"
     }
-  }
+}
