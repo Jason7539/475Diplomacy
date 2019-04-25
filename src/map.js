8f82b85daa706eceb33f9261b15c2f52f70a4288
ellenfb <44880614+ellenfb@users.noreply.github.com>
2019-04-25 14:32:58 -0700

    Implementing convoy, support, hold

--- src/map.js
+++ src/map.js
@@ -30,7 +30,7 @@ function move(){
 }
 
 function moveunit(id, dest){
-    D = document.getElementById("E")
+    //D = document.getElementById("E")
   /*  doc = D.getSVGDocument()
     troop = doc.getElementById(id)
 
@@ -41,31 +41,59 @@ function moveunit(id, dest){
    // targetX = target.getAttribute("cx")
     //troop.setAttribute("cx", targetX)
     console.log(region[0]+ " "+ current_instruction + " "+ region[1]);
-
+	region= []
 
     // store into the database 
 
 }
 
-function suportunit(id, dest){
-    console.log(region);
+function supportunit(id, dest){
+    console.log(region[0]+ " "+ current_instruction + " "+ region[1]);
+	region= []
 
 }
 
 function support() {
-    alert("click country to support")
+    /*alert("click country to support")
     current_instruction = "supports";
-    console.log(region);
+    console.log(region);*/
+	current_instruction = "supports";
+    var instruction_1 = new Object();
+    instruction_1.region_1 = region[0];
+    instruction_1.region_2 = region[1];
+    console.log(" object : " + instruction_1.region_1)
+    supportunit(region[0],region[1]);
+
+}
+
+function convoyunit(id1, id2, dest){
+    console.log(region[0]+ " "+ current_instruction + " "+ region[1] + " to " + region[2]);
+	region= []
 
 }
 
 function convoy() {
-    alert("Select army to convoy")
+    current_instruction = "convoys";
+    var instruction_1 = new Object();
+    instruction_1.region_1 = region[0];
+    instruction_1.region_2 = region[1];
+	instruction_1.region_3 = region[2];
+    console.log(" object : " + instruction_1.region_1)
+    convoyunit(region[0],region[1]);
+}
+
+function holdunit(id){
+    console.log(region[0]+ " "+ current_instruction);
+	region= []
+
 }
 
 function hold() {
-    alert("Selected country will hold")
-    current_instruction = "holds"
+    current_instruction = "holds";
+    var instruction_1 = new Object();
+    instruction_1.region_1 = region[0];
+    console.log(" object : " + instruction_1.region_1)
+    holdunit(region[0]);
 }
 
 /* for chat display */
@@ -83,7 +111,7 @@ function closeForm() {
 function getId(id){
     alert(id)
   
-    if(region.length == 2)
+    if(region.length == 3)
     {
         region= []
     }
